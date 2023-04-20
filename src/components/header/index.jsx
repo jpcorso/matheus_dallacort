b331146d4b7e736b35ba6c92ea3979a9018326f1
João Pedro Licks Corso <jp.corso123@gmail.com>
2023-04-20 00:59:56 -0300

    esqueleto do header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,6 +3,11 @@ import { Link} from 'react-router-dom';
 
 export default function Header() {
     return (
-        <Link to="https://www.google.com" className={style.text}>Matheus Dallacort</Link>
+        <div className={style.boxHeader}>
+            <Link to="https://www.google.com" className={style.text}>Matheus Dallacort</Link>
+            <Link to="https://www.google.com" className={style.text}>Cases</Link>
+            <Link to="https://www.google.com" className={style.text}>Avaliações</Link>
+            <Link to="https://www.google.com" className={style.text}>Contate-me</Link>
+        </div>
     )
 }

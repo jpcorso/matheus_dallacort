577fd137f2fc2d71d6d007d8e0737c63be963ff0
João Pedro Licks Corso <jp.corso123@gmail.com>
2023-04-27 13:04:29 -0300

    base do portfolio feita, nao responsiva ainda

---
+++ src/components/resultado_aluno/index.jsx
@@ -0,0 +1,13 @@
+import style from "../resultado_aluno/resultado_aluno.module.css"
+
+export default function Aluno({nome, antes, depois}){
+    return(
+        <div className={style.projeto}>
+            <div className={style.title}>{nome}</div>
+            <div className={style.boxFotos}>
+                <img className={style.foto} src={antes} alt="antes"/>
+                <img className={style.foto} src={depois} alt="depois"/>
+            </div>
+        </div>
+    )
+}

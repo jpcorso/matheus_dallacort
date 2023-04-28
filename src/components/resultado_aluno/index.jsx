604c584a497e57dfd6b4545df730ba805ad0c79f
João Pedro Licks Corso <jp.corso123@gmail.com>
2023-04-28 00:32:18 -0300

    esqueleto das avaliacoes

--- src/components/resultado_aluno/index.jsx
+++ src/components/resultado_aluno/index.jsx
@@ -4,9 +4,12 @@ export default function Aluno({nome, antes, depois}){
     return(
         <div className={style.projeto}>
             <div className={style.title}>{nome}</div>
-            <div className={style.boxFotos}>
-                <img className={style.foto} src={antes} alt="antes"/>
-                <img className={style.foto} src={depois} alt="depois"/>
+            <div className="fundo-projeto">
+                <div className={style.boxFotos}>
+                    <img className={style.foto} src={antes} alt="antes"/>
+                    <img className={style.foto} src={depois} alt="depois"/>
+                
+                </div>
             </div>
         </div>
     )

bf361557929eabf9b0495ff92bb6ba5840deea1f
João Pedro Licks Corso <jp.corso123@gmail.com>
2023-04-30 01:32:18 -0300

    alguns ajustes nos servicos

--- src/components/servicos/index.jsx
+++ src/components/servicos/index.jsx
@@ -1,25 +1,29 @@
 import style from "./servicos.module.css"
 
 import logo from "../../z_images/LOGO_4.png"
+import logo1 from "../../z_images/LOGO  1.png"
+import logo2 from "../../z_images/LOGO  2.png"
 
 function Servicos() {
     return (
       <div className={style.container}>
         <h1 className={style.title}>Serviços</h1>
         <div className={style.boxServicos}>
-
           <div className={style.item}>
             <img className={style.icone} src={logo}/>
-            Treinos Personalizados
+            Treinos<br/>
+            Personalizados
           </div>
           <div className={style.item}>
-            <img className={style.icone} src={logo}/>
-            Avaliações Físicas e posturais
+            <img className={style.icone} src={logo1}/>
+            Avaliações Físicas<br/>
+            e posturais
           </div>
 
           <div className={style.item}>
-            <img className={style.icone} src={logo}/>
-            Acompanhamento diário
+            <img className={style.icone} src={logo2}/>
+            Acompanhamento<br/>
+            diário
           </div>
 
         </div>

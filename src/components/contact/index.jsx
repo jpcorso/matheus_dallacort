44deb27c8f17a35fbd952a571048dbb85a08ce66
João Pedro Licks Corso <jp.corso123@gmail.com>
2023-05-01 23:39:53 -0300

    box juntado com o footer e funcional

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -2,6 +2,12 @@ import style from "./contact.module.css"
 
 import React, { useState } from "react";
 
+import logo from "../../z_images/LOGO  1.png"
+
+import instaIcon from "../../z_images/icons8-instagram.svg"
+import facebookIcon from "../../z_images/icons8-facebook.svg"
+import linkedInIcon from "../../z_images/icons8-linkedin.svg"
+
 
     const ENDPOINT = "";
 
@@ -16,48 +22,48 @@ import React, { useState } from "react";
         if (submitted){
             return(
                 <>
-                    <div className={style.container}>
-            <div className={style.title}>Contate-me</div>
-            <div className={style.boxContact}>
-                <form
-                    action={ENDPOINT}
-                    onSubmit={handleSubmit}
-                    method="POST"
-                    target="_blank"
-                    name="contact"
-                    data-netlify="true"
-                >
-                    <textarea
-                        type="text"
-                        placeholder="Nome"
-                        name="nome"
-                        className={style.textContact}
-                        required
-                    />
-                    <textarea
-                        type="text"
-                        placeholder="E-mail"
-                        name="email"
-                        className={style.textContact}
-                        required
-                    />
-                    <textarea
-                        placeholder="Mensagem"
-                        name="mensagem"
-                        className={style.mensagemContact}
-                        required
-                    />
-                    <button
-                        className={style.submitButton}
-                        type="submit"
+                <div className={style.container}>
+                    <div className={style.title}>Contate-me</div>
+                    <div className={style.boxContact}>
+                        <form
+                            action={ENDPOINT}
+                            onSubmit={handleSubmit}
+                            method="POST"
+                            target="_blank"
+                            name="contact"
+                            data-netlify="true"
                         >
-                        Enviar
-                        </button>
-                
-                </form>
-            </div>
-        </div>
-                    <div className={style.submited}>Obrigado! Entrarei em contato em breve!</div>
+                            <textarea
+                                type="text"
+                                placeholder="Nome"
+                                name="nome"
+                                className={style.textContact}
+                                required
+                            />
+                            <textarea
+                                type="text"
+                                placeholder="E-mail"
+                                name="email"
+                                className={style.textContact}
+                                required
+                            />
+                            <textarea
+                                placeholder="Mensagem"
+                                name="mensagem"
+                                className={style.mensagemContact}
+                                required
+                            />
+                            <button
+                                className={style.submitButton}
+                                type="submit"
+                                >
+                                Enviar
+                                </button>
+                        
+                        </form>
+                    </div>
+                </div>
+                <div className={style.submited}>Obrigado! Entrarei em contato em breve!</div>
                 </>
             )
         }
@@ -66,43 +72,68 @@ import React, { useState } from "react";
     return(
         <div className={style.container}>
             <p className={style.title}>Contate-me</p>
-            <div className={style.boxContact}>
-                <form
-                    action={ENDPOINT}
-                    onSubmit={handleSubmit}
-                    method="POST"
-                    target="_blank"
-                    name="contact"
-                    data-netlify="true"
-                >
-                    <textarea
-                        type="text"
-                        placeholder="Nome"
-                        name="nome"
-                        className={style.textContact}
-                        required
-                    />
-                    <textarea
-                        type="text"
-                        placeholder="E-mail"
-                        name="email"
-                        className={style.textContact}
-                        required
-                    />
-                    <textarea
-                        placeholder="Mensagem"
-                        name="mensagem"
-                        className={style.mensagemContact}
-                        required
-                    />
-                    <button
-                        className={style.submitButton}
-                        type="submit"
-                        >
-                        Enviar
-                        </button>
+            <div className={style.boxFooter}>
+                <img className={style.logo} src={logo} alt="logo"/>
+                <div className={style.boxContact}>
+                    <form
+                        action={ENDPOINT}
+                        onSubmit={handleSubmit}
+                        method="POST"
+                        target="_blank"
+                        name="contact"
+                        data-netlify="true"
+                    >
+                        <textarea
+                            type="text"
+                            placeholder="Nome"
+                            name="nome"
+                            className={style.textContact}
+                            required
+                        />
+                        <textarea
+                            type="text"
+                            placeholder="E-mail"
+                            name="email"
+                            className={style.textContact}
+                            required
+                        />
+                        <textarea
+                            placeholder="Mensagem"
+                            name="mensagem"
+                            className={style.mensagemContact}
+                            required
+                        />
+                        <button
+                            className={style.submitButton}
+                            type="submit"
+                            >
+                            Enviar
+                            </button>
+                    
+                    </form>
+                </div>
+                <div className={style.redesSociais}>
+                    <div className={style.textRedesSociais}>
+                    Me siga nas redes sociais
+                    </div>
+                    <div className={style.iconRedesSociais}>
+                        <a href="https://www.instagram.com/mateusdallacort/" alt="instagram" target="_blank">
+                            <img className={style.icon} src={instaIcon} />    {/*Instagram icon by Icons8*/}
+                        </a>
+                        <a href="https://www.facebook.com/matedallacort" alt="facebook" target="_blank">
+                            <img className={style.icon} src={facebookIcon}/>    {/*Facebook icon by Icons8*/}
+                        </a>
+                        <a href="https://www.linkedin.com/in/mateus-dallacort-561601239/?originalSubdomain=br" alt="linkedin" target="_blank">
+                            <img className={style.icon} src={linkedInIcon}/> {/*LinkedIn icon by Icons8*/}
+                        </a>
                 
-                </form>
+                    </div>
+                </div>
+                <a href="https://www.instagram.com/jp.corso/" target="_blank">
+                    <div className={style.creditos}>
+                        Feito por: @jp.corso
+                    </div>
+                </a>
             </div>
         </div>
     )

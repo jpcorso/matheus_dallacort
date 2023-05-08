d40bf3f1bf4c7cb3988ce01308a80eeff205f0e8
João Pedro Licks Corso <jp.corso123@gmail.com>
2023-05-07 21:03:02 -0300

    adicionei carousel para os videos

--- src/components/servicos/index.jsx
+++ src/components/servicos/index.jsx
@@ -5,10 +5,14 @@ import calendario from "../../z_images/gym-schedule.svg"
 import fitaMetrica from "../../z_images/measure-tape.svg"
 import halter from "../../z_images/dumbbell.svg"
 
+//carousel com videos
+import Carousel from 'react-elastic-carousel';
+import Item from './Item'
+
 function Servicos() {
     return (
       <div className={style.container}>
-        <h1 className={style.title}>Serviços</h1>
+        <div className={style.title}>Serviços</div>
         <div className={style.boxServicos}>
 
           <div className={style.item}>
@@ -32,8 +36,58 @@ function Servicos() {
           </div>
 
         </div>
+
+        <div className={style.videosBox}>
+        <div className={style.containerCarousel}>
+            <div className={style.carouselWrapper}>
+                <Carousel 
+                    itemsToShow={1} 
+                    enableAutoPlay={false}
+                    transitionMs={2000}
+                    autoPlaySpeed={8000}
+                    renderArrow={myArrow}
+                    renderPagination={myPagination}
+                    itemPadding={[10, 10]}
+                    pagination={true}
+                    isInfinite={true} // adicionado aqui
+                >
+                    <Item className={style.carouselItem}>
+              
+                    </Item>
+                    <Item className={style.carouselItem}>
+                      
+                    </Item>
+                    <Item className={style.carouselItem}>
+                      
+                    </Item>
+              
+                </Carousel>
+            </div>   
+        </div>
+        </div>
+
       </div>
     );
+
+    function myArrow({ type, onClick, isEdge }) {
+      const pointer = type === 'PREV' ? '<' : '>'
+      return (
+        <button onClick={onClick} disabled={isEdge} className={style.arrow}>{pointer}</button>
+      )
+    }
+
+    function myPagination({ pages, activePage, onClick }) {
+        return (
+          <div className={style.pagination}>
+            {pages.map(page => {
+              const isActivePage = activePage === page
+              return (
+                <button key={page} onClick={() => onClick(page)} className={isActivePage ? style.activePage : style.inactivePage}/>
+              )
+            })}
+          </div>
+        )
+    }
   }
   
   export default Servicos;
